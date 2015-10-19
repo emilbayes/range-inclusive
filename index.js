d7929ed1d950908f979af7791e4b0e2e1340b036
Emil Bay <github@tixz.dk>
2015-10-19 12:46:51 +0200

    Revise algorithm to avoid global truncation

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-module.exports = function rangeInclusive (start, stop, step) {
+module.exports = function rangeInclusive (start, stop, stepSize) {
   if (stop == null) {
     stop = start
     start = 1
   }
-  if (step == null) step = 1
+  if (stepSize == null) stepSize = 1
+
+  var steps = (stop - start) / stepSize
 
   var set = []
-  for (; (stop - start) * step >= 0; start += step) set.push(start)
+  for (var step = 0; step <= steps; step++) set.push(start + step * stepSize)
 
   return set
 }
